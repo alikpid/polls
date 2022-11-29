180c12efbff378761ff6f013c16f6630979c4c40
alikpid <koromislo.molodec@gmail.com>
2022-11-29 14:01:16 +0700

    описание и картинки к опросам

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,5 +1,6 @@
 import datetime
 
+from django.core.validators import FileExtensionValidator
 from django.db.models.signals import post_save
 from django.utils import timezone
 
@@ -11,6 +12,10 @@ from PIL import Image
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
+    post_description = models.CharField(default='', max_length=200, verbose_name='Краткое описание')
+    poll_description = models.CharField(default='', max_length=200, verbose_name='Подробное описание')
+    img = models.ImageField(default='default_poll', max_length=200, verbose_name='Картинка', upload_to='vote_pics', blank=True, null=True,
+                            validators=[FileExtensionValidator(allowed_extensions=['png', 'jpg', 'jpeg'])])
 
     def was_published_recently(self):
         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
@@ -30,7 +35,7 @@ class Choice(models.Model):
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    image = models.ImageField(default='default.jpg', upload_to='profile_pics')
+    image = models.ImageField(default='default_profile.jpg', upload_to='profile_pics')
 
     def __str__(self):
         return f'{self.user.username} Profile'
